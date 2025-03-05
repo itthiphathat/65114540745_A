79773e9ac5d4d412940b2d812ee4ef30b018a3a9
itthiphathat <itthiphat.pr.65@ubu.ac.th>
2025-03-05 13:43:56 +0700

    Delete 65114540745_A/myproject directory

--- 65114540745_A/myproject/appname/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Course(models.Model):
-    Course_id = models.CharField(max_length=128, primary_key=True)
-    Course_name = models.CharField(max_length=128)
-    Teacher_name = models.CharField(max_length=128)
--- 65114540745_A/myproject/appname/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import ListView
-from .models import Course
-
-class CourseListView(ListView): 
-    model = Course 
-    template_name = 'list_courses.html' 
-    context_object_name = 'courses'
